371da06902658b151e48f4ab78e69ac34d84f339
Edvinas09 <edvinas.bagdonas10@gmail.com>
2025-06-17 17:02:07 +0300

    Initial commit

---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+  if (authLoading) return; // Wait for auth to finish loading
+
+  if (authUser) {
+    const userRole = authUser.userRole?.toLowerCase();
+    if (
+      (userRole === "manager" && pathname.startsWith("/search")) ||
+      (userRole === "manager" && pathname === "/")
+    ) {
+      router.push("/managers/properties", { scroll: false });
+      return;
+    }
+  }
+  
+  // Set loading to false regardless of whether user is authenticated
+  setIsLoading(false);
+}, [authUser, authLoading, router, pathname]);
+
+if (authLoading || isLoading) return <>Loading...</>;
+
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+      <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
